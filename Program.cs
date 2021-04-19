1a82d4b13107d5474e145db48aef84849f521def
Andres Sainz <andresmsainz@gmail.com>
2021-04-19 17:09:11 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 2;
+            x = x + 5;
+            Console.WriteLine("x = " + x);
+
+            double dbl_num = 0.0d;
+            float flt_num = 0.0f;
+            decimal dcm_num = 0.0m;
+
+            dbl_num = 9.8d;
+            flt_num = 5.5f;
+            dcm_num = 100.250m;
+
+
+            Console.WriteLine("dbl_num = " + dbl_num);
+            Console.WriteLine("flt_num = " + flt_num);
+            Console.WriteLine("dcm_num = " + dcm_num);
+        }
+    }
+}
